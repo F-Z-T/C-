47239ea2c45323f6775720d1f51ff221c72cd839
fatma zehra turhan <61631047+F-Z-T@users.noreply.github.com>
2021-02-21 18:22:22 +0300

    Add files via upload

---
+++ Shopping.WebFormUl/UyeOl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shopping.WebFormUl
+{
+    public partial class UyeOl : Form
+    {
+        public UyeOl()
+        {
+            InitializeComponent();
+        }
+
+        private void lblSatıcıKA_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnKayıtOl_Click(object sender, EventArgs e)
+        {
+            Form2 form2sec = new Form2();
+            form2sec.Show();
+            this.Hide();
+            MessageBox.Show("Kayıt Başarılı");
+        }
+
+        private void tbxYenıKA_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+        
+    }
+   
+}
